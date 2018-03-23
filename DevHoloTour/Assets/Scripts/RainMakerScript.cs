885e8086dab502b9d6e98d1a37e5d343a31fca17
RDOE\KELL3786 <CRKeller@rdoequipment.com>
2018-03-23 10:52:05 -0500

    Added Rain and ColtonScene

---
+++ DevHoloTour/Assets/Scripts/RainMakerScript.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainMakerScript : MonoBehaviour {
+
+    private ParticleSystem rain;
+    private AudioSource audio;
+    private float fadeTime = 3f;
+    private float beginningAudioVolumeSetting;
+    private bool isRaining = false;
+
+    public void StartRaining()
+    {
+        if (!isRaining)
+        {
+            rain.Play();
+            StopAllCoroutines();
+            StartCoroutine(FadeInAudio());
+            isRaining = true;
+        }
+    }
+
+    public void StopRaining()
+    {
+        if (isRaining)
+        {
+            rain.Stop();
+            StopAllCoroutines();
+            StartCoroutine(FadeOutAudio());
+            isRaining = false;
+        }
+    }
+
+    private IEnumerator FadeInAudio()
+    {
+        audio.volume = 0;
+        audio.Play();
+
+        while (audio.volume < beginningAudioVolumeSetting)
+        {
+            audio.volume += beginningAudioVolumeSetting * Time.deltaTime / fadeTime;
+
+            yield return null;
+        }
+        audio.volume = beginningAudioVolumeSetting;
+    }
+
+    private IEnumerator FadeOutAudio()
+    {
+        while (audio.volume > 0)
+        {
+            audio.volume -= beginningAudioVolumeSetting * Time.deltaTime / fadeTime;
+
+            yield return null;
+        }
+        audio.Stop();
+        audio.volume = beginningAudioVolumeSetting;
+    }
+    
+    public void Start()
+    {
+        rain = GetComponentInChildren<ParticleSystem>();
+        rain.Stop();
+        audio = GetComponent<AudioSource>();
+        beginningAudioVolumeSetting = audio.volume;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        this.transform.eulerAngles = new Vector3(0, this.transform.eulerAngles.y, 0);
+	}
+}
---
+++ DevHoloTour/Assets/Scripts/TouchTestScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using DigitalRuby.RainMaker;
+
+public class TouchTestScript : MonoBehaviour, IInputClickHandler{
+
+    public GameObject thing;
+    private RainScript script;
+    private bool rainOn = true;
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        Debug.Log("clicky", this);
+        if (rainOn)
+        {
+            script.RainIntensity = 0;
+            script.EnableWind = false;
+            rainOn = false;
+        }
+        else
+        {
+            script.RainIntensity = 1;
+            script.EnableWind = true;
+            rainOn = true;
+        }
+
+    }
+
+    // Use this for initialization
+    void Start () {
+        script = thing.GetComponent<RainScript>();
+        if (script)
+        {
+            Debug.Log("Got the thing", this);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
